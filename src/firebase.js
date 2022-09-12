8f69d1ad17f3c27c8d7a6bb50f9eefcb49877f5c
EvanAristy <evanjaristy@gmail.com>
2022-09-12 17:28:30 -0400

    setting up firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAfCFDSK8n7ELb5s47ZXOYnldODkwT4eLs",
+    authDomain: "prime-clone-4bf97.firebaseapp.com",
+    projectId: "prime-clone-4bf97",
+    storageBucket: "prime-clone-4bf97.appspot.com",
+    messagingSenderId: "778756183115",
+    appId: "1:778756183115:web:bd0cf8b848f758c1b129ce",
+    measurementId: "G-XZDWYKL9TL"
+  };
