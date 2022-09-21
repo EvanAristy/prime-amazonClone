94516f7c96349659e2384ce3807c10998058121e
EvanAristy <evanjaristy@gmail.com>
2022-09-21 17:23:36 -0400

    installed browserRouter and made the checkout page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import {  Route, Routes } from 'react-router-dom';
+// Css
 import './App.css';
 // Components
 import Header from './Header';
 import Home from './Home';
+import Checkout from './Checkout';
 
 function App() {
   return (
     // BEM
-    <div className="app">
-     <Header />
-     <Home />
-    </div>
+    
+      <div className="app">
+        
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/checkout' element={<Checkout />} />
+        </Routes>
+        
+      </div>
+    
   );
 }
 
