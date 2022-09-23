2a297f3eb2e282e1bb97bfed637d7368e091cfe4
EvanAristy <evanjaristy@gmail.com>
2022-09-23 18:24:47 -0400

    Added the subtotal componenet and styled it

--- src/Components/Subtotal/index.jsx
+++ src/Components/Subtotal/index.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import { SportsBasketball } from "@mui/icons-material";
+import React from "react";
+import CurrencyFormat from "react-currency-format";
 // Css
-import "./styles.css"
+import "./styles.css";
 
 const Subtotal = () => {
-    return (
-        <div className='subtotal'>
-            
-        </div>
-    );
-}
+  return (
+    <div className="subtotal">
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+                {/* hw */}
+              Subtotal (0 items):
+              <strong>0</strong>
+            </p>
+            <small className="subtotal__gift">
+              <input type="checkbox" /> This order contains a gift
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        // hw
+        value={0} 
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"$"}
+      />
+
+      <button>Proceed to Checkout</button>
+    </div>
+  );
+};
 
 export default Subtotal;
