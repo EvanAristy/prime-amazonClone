fbc4f5bea99b79ea163bcb9bb97826618dc64265
EvanAristy <evanjaristy@gmail.com>
2022-09-28 17:57:10 -0400

    fixed the add to basket function

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -4,7 +4,7 @@ import React, { createContext, useContext, useReducer } from "react";
 export const StateContext = createContext();
 
 // Wrap the app and provide the Data Layer
-export const StatProvider = ({ reducer, initialState, children }) => (
+export const StateProvider = ({ reducer, initialState, children }) => (
     <StateContext.Provider value={useReducer(reducer, initialState)}>
         {children} 
     </StateContext.Provider>
