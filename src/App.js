e60a1ff4208cf81cb0636360af36f2b4ab72dc1e
EvanAristy <evanjaristy@gmail.com>
2022-11-03 17:35:14 -0400

    Working on the Stripe payment

--- src/App.js
+++ src/App.js
@@ -1,58 +1,59 @@
-import React, { useEffect } from 'react';
-import {  Route, Routes } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
 // Css
-import './App.css';
+import "./App.css";
 // Components
-import Header from './Components/Header';
-import Home from './Pages/Home';
-import Checkout from './Pages/Checkout';
-import Login from './Components/Login';
-import Payment from './Pages/Payment';
-import { auth } from './firebase';
-import { useStateValue } from './StateProvider';
+import Header from "./Components/Header";
+import Home from "./Pages/Home";
+import Checkout from "./Pages/Checkout";
+import Login from "./Components/Login";
+import Payment from "./Pages/Payment";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
+// Stripe
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  "pk_test_51M0ABkClSnzXc2In80311P14DVEpSwTjJfrqcY1tKNNzif1cvIS1HsMa4LBqojbmzYbSG5gUI2n3YvqyM5n408mm00gFVeyoo8"
+);
 
 function App() {
-
   const [{}, dispatch] = useStateValue();
 
-
-  
   useEffect(() => {
     // will only run once when the app component loads...
-    auth.onAuthStateChanged(authUser => {
-      console.log('the user is ', authUser);
+    auth.onAuthStateChanged((authUser) => {
+      console.log("the user is ", authUser);
 
       if (authUser) {
         // the user just logged in / the user was logged in
         dispatch({
-          type: 'SET_USER',
-          user: authUser
-        })
+          type: "SET_USER",
+          user: authUser,
+        });
       } else {
         // the user is logged out
         dispatch({
-          type: 'SET_USER',
-          user: null
-        })
+          type: "SET_USER",
+          user: null,
+        });
       }
-    })
-  }, [])
+    });
+  }, []);
 
   return (
     // BEM
-    
-      <div className="app">
-        
-        <Header />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/checkout' element={<Checkout />} />
-          <Route path="/payment" element={<Payment />} />
-        </Routes>
-        
-      </div>
-    
+
+    <div className="app">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/payment" element={<Elements stripe={promise}><Payment /></Elements>} />
+      </Routes>
+    </div>
   );
 }
 
--- src/Pages/Payment/index.jsx
+++ src/Pages/Payment/index.jsx
@@ -1,14 +1,45 @@
-import { Title } from '@mui/icons-material';
-import React from 'react';
+// import { Title } from '@mui/icons-material';
+import React, { useState } from 'react';
 import { useStateValue } from "../../StateProvider"
 import { Link } from 'react-router-dom';
 // Css
 import "./styles.css"
 // Components
 import CheckoutProduct from '../../Components/CheckoutProduct';
+import CurrencyFormat from 'react-currency-format';
+// Stripe Payments
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+
+
 
 const Payment = () => {
     const [{ basket, user }, dispatch] = useStateValue();
+
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const [error, setError] = useState(null);
+    const [disabled, setDisabled] = useState(true);
+
+    const handleSubmit = e => {
+        // Here is where the stripe related things go
+    }
+
+    const handleChange = e => {
+        // Listen for changes in the CardElement
+        // And display any errors as the customer types their card
+        setDisabled(e.empty);
+        setError(e.error ? e.error.message : "");
+    }
+
+    const getTotal = () => {
+        let total = 0;
+    
+        for (let i = 0; i < basket.length; i++){
+          total += basket[i].price
+        }
+        return total;
+      }
 
     return (
         <div className='payment'>
@@ -54,7 +85,24 @@ const Payment = () => {
 
                     <div className="payment__details">
                         {/* Stripe magic (payments) */}
+                        <form onSubmit={handleSubmit}>
+                            <CardElement onChange={handleChange} />
 
+                            <div className="payment__priceContainer">
+                                <CurrencyFormat 
+                                renderText={(value) => (
+                                    <>
+                                    <h3>Order Total: {value}</h3>
+                                    </>
+                                )}
+                                decimalScale={2}
+                                value={getTotal()} 
+                                displayType={"text"}
+                                thousandSeparator={true}
+                                prefix={"$"}
+                                />
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
