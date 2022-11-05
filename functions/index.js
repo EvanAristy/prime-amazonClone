dacf74f21ebd6edb0adcf8407e6ef3cbaf35ba7a
EvanAristy <evanjaristy@gmail.com>
2022-11-05 12:17:52 -0400

    Starting backend. This commit is a test to see if I have to use a seperate repo for the backend using firebase

---
+++ functions/index.js
@@ -0,0 +1,31 @@
+const functions = require("firebase-functions");
+const express = require("express")
+const cors = require("cors")
+const stripe = require("stripe")("sk_test_51M0ABkClSnzXc2InO27bElbzkhwqS3mhLrskB8gb61Zqgmy5E3gY5ZEscKDm4x2fFSsVg0dzDQBMHvpvGhhpX4ka00ElPgo4zW")
+
+// API
+
+// - App config
+
+// - Middlewares
+
+// - API routes
+
+// - Listen command
+
+
+
+
+
+
+
+
+
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
